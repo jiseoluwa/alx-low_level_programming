b2147089d9494a160c45ccf6b36879f2b1924697
Ade  Jise <adejise@Ades-MacBook-Air.local>
2022-06-06 22:51:14 +0100

    fixed linter error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
  #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return:must  Always be  0 (Success)
+ */
 int  main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
