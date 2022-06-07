8599f86fd8c88ad6864eddae0e2fe42cc3992cb2
Ade  Jise <adejise@Ades-MacBook-Air.local>
2022-06-07 18:16:42 +0100

    tired

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,12 @@
 #include "main.h"
 
 /**
-* main - print the word "_putchar"
-* Return:  0 for Success
+ *main -Entry point
+ *
+ *return: Always 0 (success)
  */
 int main(void)
 {
-	int str[] = {95, 212, 117, 116,899, 104, 97, 114};
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
-	return (0);
+  write(1,"_putcher",'\n');
+  return (0);
 }
