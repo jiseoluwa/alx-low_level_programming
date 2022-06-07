9ac32dba7ec97caceba3477533062b00817f2ef1
Ade  Jise <adejise@Ades-MacBook-Air.local>
2022-06-07 20:25:16 +0100

    void

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,11 +1,6 @@
-#include "main.h"
-/**
- *main - check the code
- *
- *Return: Always 0.
- */
-int main(void)
-{
-  print_alphabet();
-  return (0);
-}
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+
+
+#endif
