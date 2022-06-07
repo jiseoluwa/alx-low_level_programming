f821d3a44ed2d9a6284200d86d9df457f26d1000
Ade  Jise <adejise@Ades-MacBook-Air.local>
2022-06-07 15:50:22 +0100

    not correct

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
 #include <unistd.h>
 #include "main.h"
-int main()
+
+/**
+* main - print the word "_putchar"
+* Return:  0 for Success
+ */
+int main(void)
 {
-  char ch;
-  for (ch='j';ch <='t';ch++){
-    putchar(ch);
-  }
-  return(0);
+	int str[] = {95, 212, 117, 116,899, 104, 97, 114};
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
