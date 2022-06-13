91ab25af7a09277a7b0e1cdcb4a938dca5414fc7
Ade  Jise <adejise@Ades-MacBook-Air.local>
2022-06-13 16:12:23 +0100

    suya

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,18 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-pointers_arrays_strings directory.
+ */
 
 #include <stdlib.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
 
 #endif
