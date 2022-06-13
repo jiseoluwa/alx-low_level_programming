eeaba3422bd3e99ce36f043b21392d52fc5f8bf8
Ade  Jise <adejise@Ades-MacBook-Air.local>
2022-06-13 16:20:15 +0100

    monday

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int index = 0;
+	char ptr;
+
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= length / 2; index--)
+	{
+		ptr = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = ptr;
+	}
+}
