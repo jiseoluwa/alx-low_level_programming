fb68be7827249a30323d73f16f1b3145fd764d77
Ade  Jise <adejise@Ades-MacBook-Air.local>
2022-06-15 19:36:24 +0100

    need a new tutor

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
 
-#include <stdlib.h>
-
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -17,6 +9,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
 
 #endif
